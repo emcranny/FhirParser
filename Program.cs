774e4d0ab81e0e60cb9634a66ce45084f46a4858
emcranny <ecranny@hotmail.com>
2024-07-10 09:55:18 -0700

    Initial commit

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("example parsing with HL7.Fhir.R5");
+        
         Console.WriteLine("documentation at https://docs.fire.ly/");
 
         var patient = new PatientResource();
